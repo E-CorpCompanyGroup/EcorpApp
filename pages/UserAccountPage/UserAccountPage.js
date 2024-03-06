1979859f01516c606cbb9ce90a17edeb5b0b0220
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2024-03-05 19:00:07 -0800

    Add files via upload

--- pages/UserAccountPage/UserAccountPage.js
+++ pages/UserAccountPage/UserAccountPage.js
@@ -43,6 +43,49 @@ const USERACCOUNTPAGE=()=>{
     const MyName=document.querySelector('.MyName');
     STYLED(MyName,'color','White');
 
+    const USERDATA='/Database/Users.json'
+
+    LOADER(HomeDiv,'')
+
+    GETPACKAGE(USERDATA,'cors',(data)=>{
+        FINDER(data,'UserName',localStorage.getItem('Admin'),(user)=>{
+
+            VIEW(HomeDiv,'Position')
+            const Position=document.querySelector('.Position');
+            STYLED(Position,'background','#000000');
+            STYLED(Position,'height','100px');
+        
+            TEXT(Position,'h3','Position','PositionHeld',()=>{})
+            const PositionHeld=document.querySelector('.PositionHeld');
+            STYLED(PositionHeld,'color','White');
+            STYLED(PositionHeld,'textAlign','left');
+            STYLED(PositionHeld,'marginLeft','1rem');
+            STYLED(PositionHeld,'top','1rem');
+            
+            TEXT(Position,'h2',`${user.Position}`,'ActualPostion',()=>{})
+            const ActualPostion=document.querySelector('.ActualPostion');
+            STYLED(ActualPostion,'color','White');
+
+            VIEW(HomeDiv,'CurrentLocationHolder')
+            const CurrentLocationHolder=document.querySelector('.CurrentLocationHolder');
+            STYLED(CurrentLocationHolder,'background','#000000');
+            STYLED(CurrentLocationHolder,'height','100px');
+        
+            TEXT(CurrentLocationHolder,'h3','Current Location','CurrentLocationHold',()=>{})
+            const CurrentLocationHold=document.querySelector('.CurrentLocationHold');
+            STYLED(CurrentLocationHold,'color','White');
+            STYLED(CurrentLocationHold,'textAlign','left');
+            STYLED(CurrentLocationHold,'marginLeft','1rem');
+            STYLED(CurrentLocationHold,'top','1rem');
+            
+            TEXT(CurrentLocationHolder,'h2',`${user.CurrentLocation}`,'CurrentLocation',()=>{})
+            const CurrentLocation=document.querySelector('.CurrentLocation');
+            STYLED(CurrentLocation,'color','White');
+          
+        
+        })
+    })
+
 }
 
 export{USERACCOUNTPAGE}
